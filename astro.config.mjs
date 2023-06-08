b28e887bc356779250c625181fe86537da615707
Jonas <jonasg@umich.edu>
2023-06-08 19:58:31 -0400

    basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://arael34.github.io",
+  base: "/portfolio",
   integrations: [tailwind()]
 });

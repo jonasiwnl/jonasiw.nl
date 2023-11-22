7902fc65c8e7f1735f02a68bee463970d1f16564
arael <jonasg@umich.edu>
2023-11-22 13:54:47 -0500

    clean package manager and update Education seciton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jonasiwnl.github.io",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [ tailwind(), ],
   experimental: { assets: true },
 });

bb4d34768bfb7d6dd2078d9dfc4dca4acf584e4c
Jonas <jonasg@umich.edu>
2026-08-13 22:56:54 -0700

    Migrate to modern Astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
-
-// Replace `astro/client` with `@astrojs/image/client`
-/// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client" />
